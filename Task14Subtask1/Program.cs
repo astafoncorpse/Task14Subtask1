cc98f4f775e503ec9dc5c2f34917f640e86ac8b0
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2023-02-26 16:35:28 +0300

    Добавьте файлы проекта.

---
+++ Task14Subtask1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Task14Subtask1;
+
+namespace PhoneBook
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            // Сортировка сначала по имени, а затем - по фамилии
+            var sortedContact = from c in phoneBook orderby c.Name, c.LastName select c;
+
+             foreach (var contact in sortedContact)
+             Console.WriteLine(contact. Name + ", " + contact.LastName);
+        }
+    }
+}
